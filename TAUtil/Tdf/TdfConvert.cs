1909ca839886c9308e6c5678841e411e2f5d7256
ArmouredFish <armouredfish@gmail.com>
2015-01-21 13:53:52 +0000

    Add TdfConvert class for processing TDF string values

---
+++ TAUtil/Tdf/TdfConvert.cs
@@ -0,0 +1,92 @@
+﻿namespace TAUtil.Tdf
+{
+    using System;
+
+    /// <summary>
+    /// Contains utility methods for converting to and from
+    /// TDF string values.
+    /// </summary>
+    public static class TdfConvert
+    {
+        /// <summary>
+        /// Converts the given integer to a string.
+        /// </summary>
+        /// <param name="i">The integer to convert.</param>
+        /// <returns>The string representation of the integer.</returns>
+        public static string ToString(int i)
+        {
+            return Convert.ToString(i);
+        }
+
+        /// <summary>
+        /// Converts the given boolean value to a string.
+        /// </summary>
+        /// <param name="b">
+        /// The boolean value to convert.
+        /// </param>
+        /// <returns>
+        /// The string representation of the boolean value.
+        /// </returns>
+        public static string ToString(bool b)
+        {
+            return b ? "1" : "0";
+        }
+
+        /// <summary>
+        /// Converts the given double-precision floating-point value to a string.
+        /// </summary>
+        /// <param name="d">
+        /// The double to convert.
+        /// </param>
+        /// <returns>
+        /// The string representation of the double.
+        /// </returns>
+        public static string ToString(double d)
+        {
+            return Convert.ToString(d);
+        }
+
+        /// <summary>
+        /// Converts the given string representation of a boolean value
+        /// to a raw boolean value.
+        /// </summary>
+        /// <param name="s">The string to convert.</param>
+        /// <returns>The boolean value represented by the string.</returns>
+        public static bool ToBool(string s)
+        {
+            return !(string.IsNullOrEmpty(s) || s == "0");
+        }
+
+        /// <summary>
+        /// Converts the given string representation of an integer value
+        /// to an integer.
+        /// </summary>
+        /// <param name="s">The string to convert.</param>
+        /// <returns>The integer value represented by the string.</returns>
+        public static int ToInt32(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return 0;
+            }
+
+            return Convert.ToInt32(s);
+        }
+
+        /// <summary>
+        /// Converts the given string representation of a decimal number
+        /// to a double-precision floating-point value.
+        /// </summary>
+        /// <param name="s">The string to convert.</param>
+        /// <returns>The double value represented by the string.</returns>
+        public static double ToDouble(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return 0.0;
+            }
+
+            return Convert.ToDouble(s);
+        }
+    }
+}
