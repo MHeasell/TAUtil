beb3de86dcd2e5bf86ce3a94089cf6cd69daef66
ArmouredFish <armouredfish@gmail.com>
2017-12-03 01:32:39 +0000

    Add HpiTester app

---
+++ HpiTester/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HpiTester
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using TAUtil.Hpi2;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = args[0];
+            var archive = new HpiArchive(filename);
+        }
+    }
+}
