bb2ccad4c6e3ea30159be54816179ed8b80677db
ArmouredFish <armouredfish@gmail.com>
2017-12-03 13:15:32 +0000

    Polish HpiTester program

--- HpiTester/Program.cs
+++ HpiTester/Program.cs
@@ -1,18 +1,81 @@
 ﻿namespace HpiTester
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.IO;
     using TAUtil.Hpi2;
 
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var filename = args[0];
+            var command = args[0];
+
+            switch (command)
+            {
+                case "list":
+                    ListHpi(args[1]);
+                    break;
+                case "extract":
+                    ExtractHpi(args[1], args[2], args[3]);
+                    break;
+            }
+        }
+
+        private static void ListHpi(string filename)
+        {
             var archive = new HpiArchive(filename);
+            var root = archive.GetRoot();
+
+            foreach (var e in root.Entries)
+            {
+                PrintHpiEntry(e, 0);
+            }
+        }
+
+        private static void PrintHpiEntry(HpiArchive.DirectoryEntry e, int level)
+        {
+            if (e is HpiArchive.FileInfo)
+            {
+                PrintHpiEntry((HpiArchive.FileInfo)e, level);
+                return;
+            }
+
+            if (e is HpiArchive.DirectoryInfo)
+            {
+                PrintHpiEntry((HpiArchive.DirectoryInfo)e, level);
+                return;
+            }
+
+            throw new Exception("Unknown entry class");
+        }
+
+        private static void PrintHpiEntry(HpiArchive.FileInfo f, int level)
+        {
+            Console.Write(new string(' ', level));
+            Console.WriteLine("{0} {1} {2}", f.Name, f.Size, f.CompressionScheme);
+        }
+
+        private static void PrintHpiEntry(HpiArchive.DirectoryInfo d, int level)
+        {
+            Console.Write(new string(' ', level));
+            Console.WriteLine("{0} directory", d.Name);
+            foreach (var e in d.Entries)
+            {
+                PrintHpiEntry(e, level + 4);
+            }
+        }
+
+        private static void ExtractHpi(string hpiPath, string filePath, string outputPath)
+        {
+            var archive = new HpiArchive(hpiPath);
+            var fileInfo = archive.FindFile(filePath);
+            var buffer = new byte[fileInfo.Size];
+            archive.Extract(fileInfo, buffer);
+
+            using (var output = File.OpenWrite(outputPath))
+            {
+                output.Write(buffer, 0, buffer.Length);
+            }
         }
     }
 }
